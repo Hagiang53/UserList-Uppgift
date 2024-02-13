e1431d0dae032d60f2ac64cce7cc05e9663f11d2
Hagiang53 <nghagiang53@gmail.com>
2024-02-13 17:01:48 +0100

    Uppgift Kurs 1

--- Main.java
+++ Main.java
@@ -2,12 +2,16 @@
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         // Skapa en UserList för att hantera användare.
         UserList userList = new UserList();
 
         // Lägg till några användare.
+
         userList.addUser(new User(1, "", ""));
         userList.addUser(new User(2, "", ""));
         userList.addUser(new User(3, "", ""));
